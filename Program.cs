23cd9dae6b1d99a1ad76b4441acbc34476fd4268
Guilherme Sousa Oliveira <guilhermesousa1110@gmail.com>
2023-03-29 10:39:33 -0300

    Criacao de um sistema de média escolar

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write($"Digite o seu nome: ");
+string? nome = Console.ReadLine();
+
+Console.WriteLine($"Bem vindo ao sistema escolar {nome}!");
+
+Console.WriteLine($"Digite sua nota em Matemática:");
+float mat = float.Parse(Console.ReadLine());
+
+Console.WriteLine($"Digite sua nota em Portugues:");
+float port = float.Parse(Console.ReadLine());
+
+Console.WriteLine($"Digite sua nota em Geografia:");
+float geo = float.Parse(Console.ReadLine());
+
+Console.WriteLine($"Digite sua nota em Fisíca:");
+float fis = float.Parse(Console.ReadLine());
+
+Console.WriteLine($"Digite sua nota em Quimíca:");
+float quim = float.Parse(Console.ReadLine());
+
+float media = (mat + port + geo + fis + quim) / 5;
+
+Console.WriteLine($"{nome}, a sua média nesse bimestre foi de {media}!");
